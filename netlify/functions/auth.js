e1e3347ea13fb47e2c098ce02aaf7f7873329b9c
m-camara <m.camaradwwm@gmail.com>
2025-07-24 09:46:25 +0200

    fix syntax error

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -16,7 +16,7 @@ const CONFIG = {
 // Headers CORS
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers' 'Content-Type, Authorization',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Content-Type': 'application/json'
 };
